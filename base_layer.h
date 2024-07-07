9617c16bc6ecfb4ca71d3e230437744e527fe294
Nico Higgs <nicoreekohiggs@gmail.com>
2024-07-07 13:31:54 -0600

    starting a base layer

--- base_layer.h
+++ base_layer.h
@@ -1,3 +1,18 @@
 // some context OS and compiler flags here
 
+// helper macros
+
+#define Stmnt(S)                                                               \
+  do {                                                                         \
+    S                                                                          \
+  } while (0)
+
+#if !defined(AssertBreak)
+#define AssertBreak() (*(int *)0 = 0) // "crash" eventually add some output
+#endif
+
+#if ENABLE_ASSERT
 #define Assert(c) Stmnt(if (!(c)) { AssertBreak(); })
+#else
+#define Assert(c)
+#endif
